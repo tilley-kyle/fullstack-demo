3400fd830e86cde8044278d24dd6c3ad3e2a31c8
VincentCSS <VincentHTML@gmail.com>
2020-01-05 21:24:41 -0700

    initial file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
+import Nav from './Nav.jsx';
 import '../styles/App.scss';
+import BugTile from './BugTile.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       filter: 'None',
-      bugs: 'Hello World',
+      bugs: [
+        {
+          bugName: '#1',
+          bugDescription: 'Proptype validation warning for Nav component',
+          reportedBy: 'Bailey',
+          createdDate: '1/5/2020',
+          assignedTo: 'Teddi',
+          threatLevel: 'Critical',
+        },
+      ],
     };
+  }
+
+  filterHandler(filter) {
+    this.setState({ filter });
   }
 
   render() {
     return (
       <div>
-        {this.state.bugs}
+        <Nav
+          filter={this.state.filter}
+          filterHandler={this.filterHandler}
+        />
+        {this.state.bugs.map((bug) => (
+          <BugTile
+            bugName={bug.bugName}
+            bugDescription={bug.bugDescription}
+            reportedBy={bug.reportedBy}
+            createdDate={bug.createdDate}
+            assignedTo={bug.assignedTo}
+            threatLevel={bug.threatLevel}
+            key={bug.bugName}
+          />
+        ))}
       </div>
     );
   }
