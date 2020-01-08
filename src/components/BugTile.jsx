962bfe9d2e620472bea276633c5c72aae82a8f93
VincentCSS <VincentHTML@gmail.com>
2020-01-07 19:18:46 -0700

    fixed tr warning

--- src/components/BugTile.jsx
+++ src/components/BugTile.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const BugTile = (props) => (
-  <tr className="bugTile">
-    <td>{props.bugName}</td>
-    <td>{props.bugDescription}</td>
-    <td>{props.reportedBy}</td>
-    <td>{props.createdDate}</td>
-    <td>{props.assignedTo}</td>
-    <td>{props.threatLevel}</td>
-  </tr>
+  <tbody>
+    <tr className="bugTile">
+      <td>{props.bugName}</td>
+      <td>{props.bugDescription}</td>
+      <td>{props.reportedBy}</td>
+      <td>{props.createdDate}</td>
+      <td>{props.assignedTo}</td>
+      <td>{props.threatLevel}</td>
+    </tr>
+  </tbody>
 );
 
 export default BugTile;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
 const Nav = (props) => (
-  <tr>
-    <th>Bug #</th>
-    <th>Description</th>
-    <th>Reported By</th>
-    <th>Creation Date</th>
-    <th>Assigned To</th>
-    <th>
-      <label>Threat Level &nbsp;</label>
-      <select id="myList" onChange={(e) => props.filterHandler(e.target.value)}>
-        <option value="None"></option>
-        <option value="Low-Priority">Low-Priority</option>
-        <option value="Important">Important</option>
-        <option value="Critical">Critical</option>
-      </select>
-    </th>
-  </tr>
+  <thead>
+    <tr>
+      <th>Bug #</th>
+      <th>Description</th>
+      <th>Reported By</th>
+      <th>Creation Date</th>
+      <th>Assigned To</th>
+      <th>
+        <label>Threat Level &nbsp;</label>
+        <select id="myList" onChange={(e) => props.filterHandler(e.target.value)}>
+          <option value="None"></option>
+          <option value="Low-Priority">Low-Priority</option>
+          <option value="Important">Important</option>
+          <option value="Critical">Critical</option>
+        </select>
+      </th>
+    </tr>
+  </thead>
 );
 
 export default Nav;
