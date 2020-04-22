c89e4c2eae889c96d28ec924b1e23fcf8daf8037
Kyle Tilley <katilley10@gmail.com>
2020-04-22 16:06:28 -0600

    shcema for mongo

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,1 +1,17 @@
-// create db
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/bugCollection', { userNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.log('You are NOT the Love Delicatessen'));
+db.once('open', () {
+  console.log('You ARE the Love Delicatessen');
+});
+
+const bugSchema = new mongoose.Schema({
+  bugName: Number,
+  bugDescription: String,
+  reportedBy: String,
+  createdDate: String,
+  assignedTo: String,
+  threatLevel: String,
+});
