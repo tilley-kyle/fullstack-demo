94bc45d37ddac0b91f95e03aa092da79f588e866
Kyle Tilley <katilley10@gmail.com>
2020-04-22 13:54:08 -0600

    renamed input fields

--- src/components/NewBug.jsx
+++ src/components/NewBug.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const NewBug = (props) => (
   <tbody>
   <tr className="bugTile">
-    <td><input name="bug" type="text" placeholder="New Bug" /></td>
-    <td><input name="description" type="text" placeholder="Bug Description" /></td>
-    <td><input name="reported" type="text" placeholder="Reported By" /></td>
-    <td><input name="date" type="text" placeholder="Created Date" /></td>
-    <td><input name="assigned" type="text" placeholder="Assigned To" /></td>
+    <td><input name="bugName" type="text" placeholder="New Bug" /></td>
+    <td><input name="bugDescription" type="text" placeholder="Bug Description" /></td>
+    <td><input name="reportedBy" type="text" placeholder="Reported By" /></td>
+    <td><input name="creadDate" type="text" placeholder="Created Date" /></td>
+    <td><input name="assignedTo" type="text" placeholder="Assigned To" /></td>
     <td>
       <label>Threat Level &nbsp;</label>
-        <select name="threat" id="myList" onChange={(e) => props.filterHandler(e.target.value)}>
+        <select name="threatLevel" id="myList" onChange={(e) => props.filterHandler(e.target.value)}>
           <option value="None"></option>
           <option value="Low-Priority">Low-Priority</option>
           <option value="Important">Important</option>
